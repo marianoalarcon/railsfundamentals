e9c1dae36a6a00d5a36c0d74c78378000d464892
Mariano Alarcon <mariano.alarcon@hotamail.com>
2021-01-22 18:14:23 -0300

    Create edit method, continue to delete in Articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-	get 'articles/new'
-	get 'articles/index'
-  get 'articles/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	get 'articles/new', to: 'articles#new'
+	get 'articles/:id', to: 'articles#show'
+	get 'articles/:id/edit', to: 'articles#edit'
+	post 'articles', to: 'articles#create'
+	patch 'articles/:id', to: 'articles#update', as: :article
+	delete 'articles/:id', to: 'articles#destroy'
+	
 	# get "/bienvenida", to: "home#index"
 	root 'home#index'
 end
